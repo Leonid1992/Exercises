4e17fb253ea4cfeb8ea2f14fedfd5674cf88c06b
Leonid <leonid.vilcu@vlc2.com>
2023-08-09 16:15:22 +0200

    Text2

---
+++ src/WorkingWithText/ex2/Main.java
@@ -0,0 +1,10 @@
+package WorkingWithText.ex2;
+
+public class Main {
+    private static String text = "the cat barks";
+
+    public static void main(String[] args) {
+        System.out.println(text.replace("cat","dog"));
+    }
+}
+
