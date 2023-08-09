97e5450fe499d5c8d0cd2e1f944db0f672f4e8b9
Leonid <leonid.vilcu@vlc2.com>
2023-08-09 15:19:32 +0200

    exercise10

---
+++ src/ClassesObjectMethods/ex10/Main.java
@@ -0,0 +1,11 @@
+// How might you model a Math class to know about Euler’s number?
+package ClassesObjectMethods.ex10;
+
+public class Main {
+
+    public static final double E = Math.E;
+    public static void main(String[] args) {
+        System.out.println(Main.E);
+
+    }
+}
