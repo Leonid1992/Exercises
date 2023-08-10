fe00bac696402632ed48a84ef66f704971259366
Leonid <leonid.vilcu@vlc2.com>
2023-08-10 13:22:53 +0200

    numbers2

---
+++ src/Numbers/ex2/Main.java
@@ -0,0 +1,11 @@
+/* . What’s an alternative way you could add 2 + 4 without using “+” symbol?
+ */
+
+package Numbers.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( 2 | 4);
+        System.out.println(2 + 4);
+    }
+}
